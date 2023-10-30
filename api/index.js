93c999a7dc0ef39a61e9c29cf476ff89acd69458
Dungeon-Samara <146531594+Dungeon-Samara@users.noreply.github.com>
2023-10-30 13:17:50 +0300

    Delete api directory

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Код прокси 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = createProxyMiddleware({
-  target: 'https://target.com',
-  changeOrigin: true,
-});
